10730f6f7c19db5890d44b562815729da5a75c8c
Pawel Wujczyk <pwujczyk@hotmail.com>
2020-08-06 02:25:01 +0200

    service installer

--- ProductivityTools.AlibabaCloud.IpMonitor.WindowsService/ProjectInstaller.Designer.cs
+++ ProductivityTools.AlibabaCloud.IpMonitor.WindowsService/ProjectInstaller.Designer.cs
@@ -28,9 +28,29 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.serviceProcessInstaller1 = new System.ServiceProcess.ServiceProcessInstaller();
+            this.serviceInstaller1 = new System.ServiceProcess.ServiceInstaller();
+            // 
+            // serviceProcessInstaller1
+            // 
+            this.serviceProcessInstaller1.Password = null;
+            this.serviceProcessInstaller1.Username = null;
+            // 
+            // serviceInstaller1
+            // 
+            this.serviceInstaller1.ServiceName = "Service1";
+            // 
+            // ProjectInstaller
+            // 
+            this.Installers.AddRange(new System.Configuration.Install.Installer[] {
+            this.serviceProcessInstaller1,
+            this.serviceInstaller1});
 
         }
 
         #endregion
+
+        private System.ServiceProcess.ServiceProcessInstaller serviceProcessInstaller1;
+        private System.ServiceProcess.ServiceInstaller serviceInstaller1;
     }
 }
