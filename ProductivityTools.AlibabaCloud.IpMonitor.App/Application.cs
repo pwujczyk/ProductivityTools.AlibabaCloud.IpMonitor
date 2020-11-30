001b31910adc493ac93ba2c748b5b7e7ba50cbe1
Pawel Wujczyk <pwujczyk@hotmail.com>
2020-11-30 11:59:16 +0100

    message

--- ProductivityTools.AlibabaCloud.IpMonitor.App/Application.cs
+++ ProductivityTools.AlibabaCloud.IpMonitor.App/Application.cs
@@ -89,14 +89,14 @@ namespace ProductivityTools.AlibabaCloud.IpMonitor.App
             string currentAlibabaConfiguration = AlibabaGate.GetcurrentIpConfiguration(Domain, host);
             if (currentExternalIp == currentAlibabaConfiguration)
             {
-                SendEmail($"Current IP address ({currentExternalIp}) is the same as set up in Alibaba ({currentAlibabaConfiguration}), no action, host {host}");
+                SendEmail($"Current IP address ({currentExternalIp}) for host {host} is the same as set up in Alibaba ({currentAlibabaConfiguration}), no action.");
                 this.LastPublicAddress = currentExternalIp;
             }
             else
             {
                 alibabaGate.UpdateDnsValue(Domain, host, currentExternalIp);
                 var updatedValue = alibabaGate.GetcurrentIpConfiguration(Domain, host);
-                SendEmail($"Current IP address ({currentExternalIp})  was different than in Alibaba({currentAlibabaConfiguration}).Address updated to {updatedValue},  host {host}");
+                SendEmail($"Current IP address ({currentExternalIp}) for host {host}  was different than in Alibaba({currentAlibabaConfiguration}).Address updated to {updatedValue}.");
             }
         }
 
